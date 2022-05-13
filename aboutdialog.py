30f9916eb44f095b66333f5ea8cea1b22c1d6866
ee.kazakov <ee.kazakov@gmail.com>
2022-05-13 11:53:53 +0200

    update texts and translations

--- aboutdialog.py
+++ aboutdialog.py
@@ -40,7 +40,7 @@ class AboutDialog(QDialog, FORM_CLASS):
 
     def getAboutText(self):
         return self.tr('<p>Easy to use queries.</p>'
-            '<p>Plugin is an alternative for Select features using expression tool. The idea is to provide less comprehensive, but more intuitive way to run simple queries.</p>'
+            '<p>EasyQuery is an alternative for Select features using expression tool. The idea is to provide less comprehensive, but more intuitive way to run simple queries.</p>'
             '<p><strong>Developers</strong>: '
             '<a href="http://nextgis.org">NextGIS</a></p>'
             '<p><strong>Homepage</strong>: '
