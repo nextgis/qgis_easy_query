fed74518ba8d3217fafa7a45a54aae689721a24b
Ivan Barsukov <i.barsukov@protonmail.com>
2024-11-21 00:19:44 +0700

    Update translation

--- src/qgis_easy_query/i18n/qgis_easy_query_ru.ts
+++ src/qgis_easy_query/i18n/qgis_easy_query_ru.ts
@@ -2,143 +2,204 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../about_dialog.py" line="220"/>
+        <source>Developers</source>
+        <translation>Разработчики</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="221"/>
+        <source>Homepage</source>
+        <translation>Домашняя страница</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="222"/>
+        <source>Please report bugs at</source>
+        <translation>Пожалуйста, сообщайте об ошибках в</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="225"/>
+        <source>Video with an overview of the plugin</source>
+        <translation>Видео с обзором плагина</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="226"/>
+        <source>Other helpful services by NextGIS</source>
+        <translation>Другие полезные сервисы от NextGIS</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="227"/>
+        <source>Convenient up-to-date data extracts for any place in the world</source>
+        <translation>Удобная выборка актуальных данных из любой точки мира</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="230"/>
+        <source>Fully featured Web GIS service</source>
+        <translation>Полнофункциональный Веб ГИС-сервис</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="215"/>
+        <source>REPORT_END</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="224"/>
+        <source>bugtracker</source>
+        <translation>багтрекер</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="75"/>
+        <source> by NextGIS</source>
+        <translation> от NextGIS</translation>
+    </message>
+</context>
+<context>
     <name>AboutDialogBase</name>
     <message>
-        <location filename="../about_dialog_base.ui" line="14"/>
+        <location filename="../ui/about_dialog_base.ui" line="14"/>
         <source>About {plugin_name}</source>
-        <translation>О {plugin_name}</translation>
+        <translation>О модуле {plugin_name}</translation>
     </message>
     <message>
-        <location filename="../about_dialog_base.ui" line="27"/>
-        <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
-        <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
+        <location filename="../ui/about_dialog_base.ui" line="33"/>
+        <source>Information</source>
+        <translation>Информация</translation>
     </message>
     <message>
-        <location filename="../about_dialog_base.ui" line="40"/>
-        <source>&lt;p&gt;{description}&lt;/p&gt;
-&lt;p&gt;{about}&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Developers:&lt;/b&gt; &lt;a href=&quot;{main_url}/{utm}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Homepage:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Video with an overview of the plugin:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
-{video_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Please report bugs at&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;bugtracker&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Other helpful services by NextGIS:
-&lt;ul&gt;&lt;li&gt;&lt;b&gt;Convenient up-to-date data extracts for any place in the world:
-&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
-&lt;li&gt;&lt;b&gt;Fully featured Web GIS service:
-&lt;a href=&quot;{main_url}/pricing-base{utm}&quot;&gt;
-{main_url}/pricing-base&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
-&lt;/p&gt;</source>
-        <translation>&lt;p&gt;{description}&lt;/p&gt;
-&lt;p&gt;{about}&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Разработчики:&lt;/b&gt; &lt;a href=&quot;{main_url}/{utm}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Домашняя страница:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Видео с обзором плагина:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
-{video_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Пожалуйста, сообщайте о багах в&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;багтрекер&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Другие полезные сервисы NextGIS:
-&lt;ul&gt;&lt;li&gt;&lt;b&gt;Удобная выборка актуальных данных из любой точки мира:
-&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
-&lt;li&gt;&lt;b&gt;Полнофункциональный веб-ГИС-сервис:
-&lt;a href=&quot;{main_url}/pricing-base{utm}&quot;&gt;
-{main_url}/pricing-base{&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
-&lt;/p&gt;</translation>
+        <location filename="../ui/about_dialog_base.ui" line="72"/>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="94"/>
+        <source>Components</source>
+        <translation>Компоненты</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="116"/>
+        <source>Contributors</source>
+        <translation>Участники</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="158"/>
+        <source>{plugin_name}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="168"/>
+        <source>Version {version}</source>
+        <translation>Версия {version}</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="203"/>
+        <source>Get involved</source>
+        <translation>Присоединяйтесь</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="54"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="../qgis_easy_query.ui" line="14"/>
+        <location filename="../ui/qgis_easy_query.ui" line="14"/>
         <source>NextGIS EasyQuery</source>
         <translation>NextGIS EasyQuery</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="26"/>
+        <location filename="../ui/qgis_easy_query.ui" line="26"/>
         <source>2. Configure conditions for query:</source>
         <translation>2. Сформулируйте условия для выборки:</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="37"/>
+        <location filename="../ui/qgis_easy_query.ui" line="37"/>
         <source>Field</source>
         <translation>Поле</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="42"/>
+        <location filename="../ui/qgis_easy_query.ui" line="42"/>
         <source>Operator</source>
         <translation>Оператор</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="47"/>
+        <location filename="../ui/qgis_easy_query.ui" line="47"/>
         <source>Value</source>
         <translation>Значение</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="55"/>
+        <location filename="../ui/qgis_easy_query.ui" line="55"/>
         <source>Add condition</source>
         <translation>Добавить условие</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="62"/>
+        <location filename="../ui/qgis_easy_query.ui" line="62"/>
         <source>Delete condition</source>
         <translation>Удалить условие</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="75"/>
+        <location filename="../ui/qgis_easy_query.ui" line="75"/>
         <source>3. Select query mode:</source>
         <translation>3. Выберите режим выборки:</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="91"/>
+        <location filename="../ui/qgis_easy_query.ui" line="91"/>
         <source>4. Select type of result:</source>
         <translation>4. Выберите результат выборки:</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="114"/>
+        <location filename="../ui/qgis_easy_query.ui" line="114"/>
         <source>About</source>
         <translation>О модуле</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="121"/>
+        <location filename="../ui/qgis_easy_query.ui" line="121"/>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="158"/>
+        <location filename="../ui/qgis_easy_query.ui" line="158"/>
         <source>All conditions should be met</source>
         <translation>Все условия должны выполняться</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="163"/>
+        <location filename="../ui/qgis_easy_query.ui" line="163"/>
         <source>At least one condition should be met</source>
         <translation>Хотя бы одно условие должно выполняться</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="190"/>
+        <location filename="../ui/qgis_easy_query.ui" line="190"/>
         <source>1. Select a layer for query:</source>
         <translation>1. Выберите слой для выборки:</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="214"/>
+        <location filename="../ui/qgis_easy_query.ui" line="214"/>
         <source>Feature selection</source>
         <translation>Выделение объектов в слое</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="219"/>
+        <location filename="../ui/qgis_easy_query.ui" line="219"/>
         <source>New layer with query results</source>
         <translation>Новый слой с результатами выборки</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="238"/>
+        <location filename="../ui/qgis_easy_query.ui" line="238"/>
         <source>5.Execute</source>
         <translation>5. Выполнить</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="258"/>
+        <location filename="../ui/qgis_easy_query.ui" line="258"/>
         <source>Zoom to selected features</source>
         <translation>Автоматически приблизить карту к результату выборки</translation>
     </message>
     <message>
-        <location filename="../qgis_easy_query.ui" line="224"/>
+        <location filename="../ui/qgis_easy_query.ui" line="224"/>
         <source>Apply filter to current layer</source>
         <translation>Применить фильтр к текущему слою</translation>
     </message>
@@ -146,27 +207,27 @@
 <context>
     <name>EasyQueryDialog</name>
     <message>
-        <location filename="../easy_query_dialog.py" line="222"/>
+        <location filename="../easy_query_dialog.py" line="277"/>
         <source>Attention!</source>
         <translation>Внимание!</translation>
     </message>
     <message>
-        <location filename="../easy_query_dialog.py" line="205"/>
+        <location filename="../easy_query_dialog.py" line="255"/>
         <source>Layer must be selected</source>
         <translation>Должен быть выбран слой</translation>
     </message>
     <message>
-        <location filename="../easy_query_dialog.py" line="211"/>
+        <location filename="../easy_query_dialog.py" line="262"/>
         <source>No conditions are set</source>
         <translation>Не заданы условия выборки</translation>
     </message>
     <message>
-        <location filename="../easy_query_dialog.py" line="217"/>
+        <location filename="../easy_query_dialog.py" line="269"/>
         <source>Field must be selected for all conditions</source>
         <translation>У каждого условия должно быть выбрано поле</translation>
     </message>
     <message>
-        <location filename="../easy_query_dialog.py" line="222"/>
+        <location filename="../easy_query_dialog.py" line="277"/>
         <source>Value must be selected for all conditions</source>
         <translation>У каждого условия должно быть задано значение</translation>
     </message>
